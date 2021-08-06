942cb6cdfcb5eee6293b07295b56dde76036f190
fesavali <felixnzioki99@gmail.com>
2021-08-06 15:23:14 +0300

    Full site upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2018 YR2 Codes</strong>
+    </div>
+</footer>
