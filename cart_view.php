bc3eacf00acd0c92fba13c137396d4b11390f523
fesavali <felixnzioki99@gmail.com>
2021-09-27 10:36:47 +0300

    Update cart_view.php

--- cart_view.php
+++ cart_view.php
@@ -1,5 +1,5 @@
 <?php include 'includes/session.php'; ?>
-<!-- savali codes  displays items-->
+<!-- savali codes  displays items added -->
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
